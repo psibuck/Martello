7575f0124f56f43fc201bed8727ad65a899e6c20
psibuck <psibuck@gmail.com>
2023-05-03 22:28:29 +0100

    add controller code class, add bp game mode and controller, reparent controller to cpp

---
+++ Source/Martello/Core/AMPlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+
+#include "AMPlayerController.generated.h"
+
+UCLASS()
+class AMPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
