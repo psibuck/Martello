ba3d139c8aeb47b74324cac0f13ea32b59fa11ee
psibuck <psibuck@gmail.com>
2023-05-03 21:05:03 +0100

    first commit

---
+++ Source/Martello/Martello.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Martello.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Martello, "Martello" );
---
+++ Source/Martello/MartelloGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MartelloGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARTELLO_API AMartelloGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
